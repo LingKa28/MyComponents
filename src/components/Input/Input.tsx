f30f630b128618a5652e1139e09b156ac6682122
Ying <cnfty786@gmail.com>
2022-11-10 09:24:41 +0800

    feat(Input): outline 类型基本外观

---
+++ src/components/Input/Input.tsx
@@ -0,0 +1,12 @@
+import "./index.less";
+
+const Input = () => {
+  return (
+    <div className="input-container">
+      <input />
+      <label>Lable</label>
+    </div>
+  );
+}
+
+export default Input;
